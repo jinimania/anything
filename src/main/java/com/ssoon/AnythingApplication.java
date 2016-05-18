1886a76095b8c973ec08dd34bb0096af4f8d5a7a
Soohoon Lee <jjinimania@gmail.com>
2016-05-19 01:52:42 +0900

    Initial commit

---
+++ src/main/java/com/ssoon/AnythingApplication.java
@@ -0,0 +1,12 @@
+package com.ssoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnythingApplication.class, args);
+	}
+}
